240eaff4d1b440a12852ffbc64b2dea67d1cee52
Russ Miles <russ@russmiles.com>
2013-08-12 16:19:24 +0100

    Creating initial document according to new directory structure.

--- 1/main/java/com/yummynoodlebar/core/events/orders/CreateOrderEvent.java
+++
@@ -1,6 +0,0 @@
-package com.yummynoodlebar.core.events.orders;
-
-import com.yummynoodlebar.core.events.CreateEvent;
-
-public class CreateOrderEvent extends CreateEvent {
-}
--- 2/src/main/java/com/yummynoodlebar/core/events/orders/OrderDetailsEvent.java
+++
@@ -1,6 +0,0 @@
-package com.yummynoodlebar.core.events.orders;
-
-import com.yummynoodlebar.core.events.ReadEvent;
-
-public class OrderDetailsEvent extends ReadEvent{
-}
