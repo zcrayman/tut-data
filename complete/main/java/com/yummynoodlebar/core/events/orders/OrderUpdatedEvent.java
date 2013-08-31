d69315c785cd2c7ff05f6aec90b2c8aa361015bf
David Dawson <david.dawson@simplicityitself.com>
2013-09-01 00:29:18 +0100

    Remove superflous complete

--- complete/main/java/com/yummynoodlebar/core/events/orders/OrderUpdatedEvent.java
+++
@@ -1,6 +0,0 @@
-package com.yummynoodlebar.core.events.orders;
-
-import com.yummynoodlebar.core.events.UpdatedEvent;
-
-public class OrderUpdatedEvent extends UpdatedEvent {
-}
--- complete/src/main/java/com/yummynoodlebar/core/events/orders/UpdateOrderEvent.java
+++
@@ -1,6 +0,0 @@
-package com.yummynoodlebar.core.events.orders;
-
-import com.yummynoodlebar.core.events.UpdateEvent;
-
-public class UpdateOrderEvent extends UpdateEvent {
-}
